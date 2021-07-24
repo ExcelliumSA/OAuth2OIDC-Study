2aebd7fd280d39d9588d6367a7a1005586ebca43
drighetto <drighetto@excellium-services.com>
2021-07-24 17:20:00 +0200

    Add date to mind map

--- generate_mindmap_image.sh
+++ generate_mindmap_image.sh
@@ -6,6 +6,7 @@
 work_file="/tmp/mindmap.mmd"
 echo "[+] Generate the mermaid content for the graph..."
 prefixes=(CLT API BFF STS)
+dte=$(date +'%Y%m%d')
 echo "graph LR" > $work_file
 for prefix in ${prefixes[@]}; do
 	check_count=$(grep -Ec "\-\s$prefix[0-9]{2}:" OAauth2_OIDC_Security_Validations.md)
@@ -21,7 +22,7 @@ for prefix in ${prefixes[@]}; do
 			component_name="Backend-For-Frontend"
 			;;			
 	esac
-	echo "    CHECK([Validations]) ---|$check_count checks| $prefix([$component_name])" >> $work_file
+	echo "    CHECK([Validations_$dte]) ---|$check_count checks| $prefix([$component_name])" >> $work_file
 done
 cat $work_file
 echo "[+] Generate the PNG image of the graph..."
