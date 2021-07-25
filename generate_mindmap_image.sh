24293c0d0571dc8db76d6f297501d789ac67ce68
drighetto <drighetto@excellium-services.com>
2021-07-25 09:51:50 +0200

    Fix build

--- generate_mindmap_image.sh
+++ generate_mindmap_image.sh
@@ -6,9 +6,6 @@
 work_file="/tmp/mindmap.mmd"
 echo "[+] Generate the mermaid content for the graph..."
 prefixes=(CLT API BFF STS)
-uid=$(date +'%N')
-sleep 1
-uid2=$(date +'%N')
 echo "graph LR" > $work_file
 for prefix in ${prefixes[@]}; do
 	check_count=$(grep -Ec "\-\s$prefix[0-9]{2}:" OAauth2_OIDC_Security_Validations.md)
@@ -19,16 +16,17 @@ for prefix in ${prefixes[@]}; do
 			;;
 		STS)
 			component_name="Security Token Service"
-			;;	
+			;;
 		BFF)
 			component_name="Backend-For-Frontend"
-			;;			
+			;;
 	esac
-	echo "    CHECK_$uid([Validations]) ---|$check_count checks| C$uid2_C$prefix([$component_name])" >> $work_file
+	echo "    CHECK([Validations]) ---|$check_count checks| $prefix([$component_name])" >> $work_file
 done
 cat $work_file
 echo "[+] Generate the PNG image of the graph..."
 sha256sum OAauth2_OIDC_Security_Validations.png
+echo 1 > OAauth2_OIDC_Security_Validations.png 
 npx -p @mermaid-js/mermaid-cli mmdc -i $work_file -o OAauth2_OIDC_Security_Validations.png -t forest
 sha256sum OAauth2_OIDC_Security_Validations.png
 rm $work_file
